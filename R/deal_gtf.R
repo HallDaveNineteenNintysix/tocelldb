18bb3bd98c8498f16f29ea944ed683b60139e0ef
HallDaveNineteenNintysix <42873050+HallDaveNineteenNintysix@users.noreply.github.com>
2024-01-29 22:53:41 +0800

    Add files via upload

--- R/deal_gtf.R
+++ R/deal_gtf.R
@@ -1,1 +1,17 @@
-
+#' Load a gtf file.
+#'@description
+#' Load a gtf file from ncbi and get gene & protein infomations.
+#'
+#' @param gtf_file The directory of a input gtf file.
+#'
+#' @return gene & protein informations.
+#' @export
+#'
+#' @examples NA
+deal_gtf <- function(gtf_file){
+  gtf <- rtracklayer::import(gtf_file)
+  gtf <- as.data.frame(gtf)
+  Protein <- gtf[,c("protein_id","gene")]
+  Protein <- Protein[is.na(Protein$protein_id)==F,]
+  return(Protein)
+}
