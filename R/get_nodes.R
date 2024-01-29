12c5b0b7dede4d7d0a5c2208d48cece89171ebc3
HallDaveNineteenNintysix <42873050+HallDaveNineteenNintysix@users.noreply.github.com>
2024-01-29 22:23:02 +0800

    Add files via upload

--- R/get_nodes.R
+++ R/get_nodes.R
@@ -7,21 +7,30 @@
 #' @param add_nodes adds a number of proteins with to the network based on their confidence score.
 #' @param show_query_node_labels when available use submitted names in the preferredName column when (0 or 1) (default:0).
 #' @param caller_identity your identifier for us.
+#' @param partners TRUE or FALSE(default), default retrieves only the interactions between the set of input proteins and between their closest interaction neighborhood (if add_nodes parameters is specified), interaction_partners API method provides the interactions between your set of proteins and all the other STRING proteins.
+#' @param limit 10 by default while partners set to TRUE. the number of interaction partners retrieved per protein (most confident interactions come first)
 #'
 #' @return STRINGdb interaction tsv files.
 #' @export
 #'
 #' @examples get_nodes(c("TP53","CDK2"),species=9606)
-get_nodes <- function(identifiers,species=9606,required_score=400,network_type="functional",show_query_node_labels=0,add_nodes=10,caller_identity="Anonymous"){
+get_nodes <- function(identifiers,species=9606,partners=FALSE,limit=10,required_score=400,network_type="functional",show_query_node_labels=0,add_nodes=10,caller_identity="Anonymous"){
   if(length(identifiers)>1){
     gene_identity <- paste(identifiers,sep="",collapse = "%0d")
   }
   else{
     gene_identity=identifiers
   }
-  node_url <- c("https://string-db.org/api/tsv/network?identifiers=",gene_identity,"&","species=",species,"&",
-                "required_score=",required_score,"&","network_type=",network_type,"&","add_nodes=",add_nodes,"&",
-                "show_query_node_labels=",show_query_node_labels,"&","caller_identity=",caller_identity)
+  if (partners){
+  node_url <- c("https://string-db.org/api/tsv/interaction_partners?identifiers=",gene_identity,"&species=",species,
+                "&required_score=",required_score,"&network_type=",network_type,"&caller_identity=",caller_identity,
+                "&limit=",limit)
+  }
+  else {
+    node_url <- c("https://string-db.org/api/tsv/network?identifiers=",gene_identity,"&species=",species,
+                  "&required_score=",required_score,"&network_type=",network_type,"&add_nodes=",add_nodes,
+                  "&show_query_node_labels=",show_query_node_labels,"&caller_identity=",caller_identity)
+  }
   node_url <- paste(node_url,sep = "",collapse = "")
   utils::download.file(node_url,destfile = "nodes.tsv")
   node_file <- utils::read.csv("nodes.tsv",sep = "\t")[,c(3,4)]
